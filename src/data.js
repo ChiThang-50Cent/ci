e4c08bed9454223be89b8c603b3e857127c46af6
ChiThang-50Cent <chithang.nydo@gmail.com>
2021-01-11 19:13:20 +0700

    final

--- src/data.js
+++ src/data.js
@@ -1,43 +1,53 @@
 const pokes = [
     {
         name : 'PNG82',
-        url : 'PNG82.png'
+        url : 'PNG82.png',
+        type : 'grass'
     },
     {
         name : 'PNG119',
-        url : 'PNG119.png'
+        url : 'PNG119.png',
+        type : 'water'
     },
     {
         name : 'PNG138',
-        url : 'PNG138.png'
+        url : 'PNG138.png',
+        type : 'grass'
     },
     {
         name : 'PNG141',
-        url : 'PNG141.png'
+        url : 'PNG141.png', 
+        type : 'water'
     },
     {
         name : 'PNG145',
-        url : 'PNG145.png'
+        url : 'PNG145.png',
+        type : 'fire'
     },
     {
         name : 'PNG147',
-        url : 'PNG147.png'
+        url : 'PNG147.png',
+        type : 'fire'
     },
     {
         name : 'PNG150',
-        url : 'PNG150.png'
+        url : 'PNG150.png',
+        type : 'water'
     },
     {
         name : 'PNG151',
-        url : 'PNG151.png'
+        url : 'PNG151.png',
+        type : 'fire'
     },
     {
         name : 'PNG153',
-        url : 'PNG153.png'
+        url : 'PNG153.png',
+        type : 'grass'
     },
     {
         name : 'PNG84',
-        url : 'PNG84.png'
+        url : 'PNG84.png',
+        type : 'fire'
     }
 ]
 
