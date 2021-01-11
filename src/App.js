2d8cdd8f0bf1404c1949b75e3e6d90e2a323907b
ChiThang-50Cent <chithang.nydo@gmail.com>
2021-01-11 19:24:58 +0700

    clear

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
-import './App.css';
 import React from 'react'
-import TodoItem from './component/TodoItem.jsx'
-import works from './component/data.jsx'
 
-class App extends React.Component {
-  constructor() {
-      super()
-      this.state = {
-          todos: works
-      }
-      this.handleChange = this.handleChange.bind(this)
-  }
-  
-  handleChange(id) {
-      this.setState(prevState=>{
-          const update = prevState.todos.map(todo=>{
-              if(todo.id === id) {
-                  todo.completed = !todo.completed
-              }
-              return todo
-          })
-          return {
-              todos: update
-          }
-      })
-  }
-  
-  render() {
-      const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
-      
-      return (
-          <div>
-              {todoItems}
-          </div>
-      )    
-  }
+export default function App() {
+  return (
+    <div>
+      <p>Hello MotherFucker</p>
+    </div>
+  )
 }
-
-export default App
