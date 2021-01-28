88c38f7448c1cc0665e6bb5fcf7c978ecfb4cd21
ChiThang-50Cent <chithang.nydo@gmail.com>
2021-01-28 22:21:14 +0700

    create

---
+++ src/component/head.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export function Head() {
+    return (
+        <div className='head'>
+            <div className='info'>
+                <img src="logo192.png" alt="" />
+                <div>
+                    <h3>Name: </h3>
+                </div>
+            </div>
+        </div>
+    )
+}
